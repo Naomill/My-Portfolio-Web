341b0ed144b7d2ddb832af20171c24699f6e6f34
Naomill <141665508+Naomill@users.noreply.github.com>
2024-07-12 23:00:24 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors:{
+        primaryPurple: "#8259F9",
+        secLightPurple: "#BFB4FE",
+        thirdLightPurple: "#4B3E95",
+        thirdDarkPurple: "#403877",
+        GradientLightPurple: "#A690FC",
+        GradientDarktPurple: "#461B97",
+        whiteTiltle: "#FFFFFF",
+        primaryGray: "#9F9F9F",
+        background: "#050516",
+      }
     },
   },
   plugins: [],
-};
+}
